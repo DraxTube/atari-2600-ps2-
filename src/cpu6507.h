e7adc3a1c54dd7dae5af2f4a4bef97edf175b766
DraxTube01 <draxtube01@gmail.com>
2026-02-22 15:42:28 +0100

    Update cpu6507.h

--- src/cpu6507.h
+++ src/cpu6507.h
@@ -1,32 +1,12 @@
 #ifndef CPU6507_H
 #define CPU6507_H
 
-#include <stdint.h>
+#include "types.h"
 
-typedef struct {
-    uint8_t A;      // Accumulator
-    uint8_t X;      // X Register
-    uint8_t Y;      // Y Register
-    uint8_t SP;     // Stack Pointer
-    uint16_t PC;    // Program Counter
-    uint8_t P;      // Processor Status
-    uint64_t cycles;
-} CPU6507;
-
-// Status flags
-#define FLAG_C 0x01  // Carry
-#define FLAG_Z 0x02  // Zero
-#define FLAG_I 0x04  // Interrupt Disable
-#define FLAG_D 0x08  // Decimal Mode
-#define FLAG_B 0x10  // Break
-#define FLAG_U 0x20  // Unused
-#define FLAG_V 0x40  // Overflow
-#define FLAG_N 0x80  // Negative
-
-void cpu_init(CPU6507* cpu);
-void cpu_reset(CPU6507* cpu);
-int cpu_step(CPU6507* cpu);
-uint8_t cpu_read(uint16_t addr);
-void cpu_write(uint16_t addr, uint8_t value);
+void    cpu_init(EmulatorState* emu);
+void    cpu_reset(EmulatorState* emu);
+int     cpu_step(EmulatorState* emu);
+uint8_t mem_read(EmulatorState* emu, uint16_t addr);
+void    mem_write(EmulatorState* emu, uint16_t addr, uint8_t value);
 
 #endif
