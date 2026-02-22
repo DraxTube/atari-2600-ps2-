2ca10af6e7f2cd87f87d14ac3787787db9dc68bb
DraxTube01 <draxtube01@gmail.com>
2026-02-22 15:41:46 +0100

    Update riot.h

--- src/riot.h
+++ src/riot.h
@@ -1,21 +1,12 @@
 #ifndef RIOT_H
 #define RIOT_H
 
-#include <stdint.h>
+#include "types.h"
 
-typedef struct {
-    uint8_t ram[128];
-    uint8_t porta;
-    uint8_t portb;
-    uint8_t ddra;
-    uint8_t ddrb;
-    uint16_t timer;
-    uint8_t timer_interval;
-} RIOT;
-
-void riot_init(RIOT* riot);
-uint8_t riot_read(uint16_t addr);
-void riot_write(uint16_t addr, uint8_t value);
-void riot_step(RIOT* riot, int cycles);
+void    riot_init(EmulatorState* emu);
+void    riot_reset(EmulatorState* emu);
+uint8_t riot_read(EmulatorState* emu, uint16_t addr);
+void    riot_write(EmulatorState* emu, uint16_t addr, uint8_t value);
+void    riot_tick(EmulatorState* emu, int cycles);
 
 #endif
